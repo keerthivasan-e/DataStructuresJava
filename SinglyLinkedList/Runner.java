c29a9bfef3c0ab8b51a99e6bcdd2dfc2c6e3b0a6
Keerthivasan E <57031372+keerthivasan-e@users.noreply.github.com>
2021-06-03 13:07:32 +0530

    Add files via upload

---
+++ SinglyLinkedList/Runner.java
@@ -0,0 +1,12 @@
+public class Runner {
+    
+    public static void main(String[] args){
+
+       LinkedList list = new LinkedList();
+        list.insert(5);
+        list.insert(10);
+        list.show();
+        // System.out.println("Hello world");
+    }
+
+}
